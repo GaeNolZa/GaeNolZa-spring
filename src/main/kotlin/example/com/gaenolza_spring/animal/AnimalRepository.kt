912029e8a04d31e0aa4f2c4ab2ee4f5cd1b8b9ba
invercelina <invercelina@gmail.com>
2024-08-30 14:46:55 +0900

    animal register

---
+++ src/main/kotlin/example/com/gaenolza_spring/animal/AnimalRepository.kt
@@ -0,0 +1,7 @@
+package example.com.gaenolza_spring.animal
+
+import org.springframework.data.repository.CrudRepository
+
+interface AnimalRepository : CrudRepository<Animal, Int> {
+    fun findAnimalsByCustomerId(customerId: Int): List<Animal>
+}
